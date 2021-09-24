8e481c732958297edd59d096271fd17bf8999be4
yuhyeonjae <yhj9568@naver.com>
2021-09-24 11:07:23 +0900

    [master] 외부 라이브러리 사용을 위한 설정 진행

--- Package.swift
+++ Package.swift
@@ -5,26 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "YuSPM",
+    platforms: [
+        // [주의] 외부 라이브러리가 추가된 상태에서 OS를 지정해주어야 한다
+        .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "YuSPM",
             targets: ["YuSPM"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    ], dependencies: [
+        // 외부 패키지 설정
         .package(name:"Alamofire", url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.3")),
         .package(name:"SnapKit", url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-    ],
-    targets: [
+    ], targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YuSPM",
-            dependencies: []),
+            dependencies: [
+                // 외부 패키지 리스트
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SnapKit", package: "SnapKit")
+            ]),
         .testTarget(
             name: "YuSPMTests",
             dependencies: ["YuSPM"]),
+    ], swiftLanguageVersions: [
+        // swift 언어설정
+        .v5
     ]
 )
